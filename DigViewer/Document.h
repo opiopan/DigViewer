7405cb6f0fa3410168d2c0d7ca3c7f5b1fb5e786
opiopan <opiopan@gmail.com>
2013-01-04 17:28:09 +0900

    Initial Commit

---
+++ DigViewer/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  DigViewer
+//
+//  Created by opiopan on 2013/01/04.
+//  Copyright (c) 2013年 opiopan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ DigViewer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DigViewer
+//
+//  Created by opiopan on 2013/01/04.
+//  Copyright (c) 2013年 opiopan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
